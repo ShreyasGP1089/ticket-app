a634899bc22b2dcedaeedc6c2d1133865ac760ca
ShreyasGP1089 <shreyas.g.palimar@gmail.com>
2026-01-20 03:07:26 +0530

    name change

--- backend/src/main/java/com/example/Final/config/CorsConfig.java
+++ backend/src/main/java/com/example/Final/config/CorsConfig.java
@@ -1,7 +1,5 @@
-package com.example.Final.Config;
+package com.example.Final.config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
